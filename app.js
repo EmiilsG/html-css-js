6acfa8a5ffb35e1f5714c64669de8bc4e1fe6d58
EmīlsGrīnerts <grinertsemils@gmail.com>
2025-09-03 11:22:43 +0300

    Jaa

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Sveiki no Emīla!");
+
+let skaitlis = 1;
+
+
+function izvadaSkaitli() {
+    if (skaitlis <= 10) {
+        console.log(skaitlis); 
+        skaitlis++; 
+    } else {
+        console.log("Visi skaitļi no 1 līdz 10 ir izvadīti!"); 
+    }
+}
+
+
+document.getElementById('klikšķināt').addEventListener('click', izvadaSkaitli);
